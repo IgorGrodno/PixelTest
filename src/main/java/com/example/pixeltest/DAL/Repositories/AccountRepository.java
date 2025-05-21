bed223512aed2b21231ab1b7fba05b9bedb951e5
noobprofesor <noobprofesor@gmail.com>
2025-05-21 15:07:09 +0300

    add timeout for DBlevel lock

--- src/main/java/com/example/pixeltest/DAL/Repositories/AccountRepository.java
+++ src/main/java/com/example/pixeltest/DAL/Repositories/AccountRepository.java
@@ -2,14 +2,17 @@ package com.example.pixeltest.DAL.Repositories;
 
 import com.example.pixeltest.Models.Ntities.Account;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    @QueryHints({ @QueryHint(name = "javax.persistence.lock.timeout", value = "1000") })
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT a FROM Account a WHERE a.user.id = :userId")
     Account findAccountForUpdateByUserId(@Param("userId") Long userId);
